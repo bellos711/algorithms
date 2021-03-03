7bf301ee299542805749f5bd9a87bfdec9de7db8
Kahlil Bello <bellos711@gmail.com>
2021-03-02 17:30:00 -0800

    added queue stacks file

---
+++ js_algo/stacks_queues/queue_using_stacks.js
@@ -0,0 +1,36 @@
+//idea is to put everything in last[] then push all the popped values from last[] to first[], and vice versa
+class QueueUsingStacks{
+    constructor(){
+        this.first = [];
+        this.last = [];
+    }
+
+    enqueue(value){
+        const length = this.first.length;
+        for(let i = 0; i < length; i++){
+            this.last.push(this.first.pop());
+        }
+        this.last.push(value);
+        return this;
+    }
+
+    dequeue(){
+        const length = this.last.length;
+        for(let i = 0; i < length; i++){
+            this.first.push(this.last.pop())
+        }
+        this.first.pop();
+        return this;
+    }
+
+    peek(){
+        if(this.last.length > 0 ){
+            return this.last[0]; 
+        }
+        return this.first[this.first.length-1]; //who was at the end of the index
+    }
+
+    isEmpty(){
+        return (((this.first.length === 0) && (this.last.length === 0)) ? true : false);
+    }
+}
